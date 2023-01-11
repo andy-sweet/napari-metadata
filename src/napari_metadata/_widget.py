e6ef221dd4308ffe18cd7368c311ae1fb2c916c3
Andy Sweet <andrew.d.sweet@gmail.com>
2023-01-10 16:39:07 -0800

    Show some attributes of selected layer

--- src/napari_metadata/_widget.py
+++ src/napari_metadata/_widget.py
@@ -1,37 +1,76 @@
-"""
-This module is an example of a barebones QWidget plugin for napari
-
-It implements the Widget specification.
-see: https://napari.org/stable/plugins/guides.html?#widgets
-
-Replace code below according to your needs.
-"""
 from typing import TYPE_CHECKING
 
 from magicgui import magic_factory
-from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
+from qtpy.QtWidgets import (
+    QGridLayout,
+    QLabel,
+    QLineEdit,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+)
 
 if TYPE_CHECKING:
     import napari
 
 
 class ExampleQWidget(QWidget):
-    # your QWidget.__init__ can optionally request the napari viewer instance
-    # in one of two ways:
-    # 1. use a parameter called `napari_viewer`, as done here
-    # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
-    def __init__(self, napari_viewer):
+    def __init__(self, viewer: "napari.viewer.Viewer"):
         super().__init__()
-        self.viewer = napari_viewer
+        self._value_edits = {}
+        self._restore_buttons = {}
+        self.viewer = viewer
+        self.viewer.layers.selection.events.changed.connect(
+            self._on_selected_layers_changed
+        )
 
-        btn = QPushButton("Click me!")
-        btn.clicked.connect(self._on_click)
+        layout = QVBoxLayout()
+        self.setLayout(layout)
 
-        self.setLayout(QHBoxLayout())
-        self.layout().addWidget(btn)
+        description = QLabel(
+            "View and edit metadata values, and select which to display in GUI"
+        )
+        layout.addWidget(description)
 
-    def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
+        self._attribute_widget = QWidget()
+        layout.addWidget(self._attribute_widget)
+
+        self._attribute_layout = QGridLayout()
+        self._attribute_widget.setLayout(self._attribute_layout)
+        self._add_attribute_widgets("name")
+        self._add_attribute_widgets("scale", editable=True)
+
+        self._on_selected_layers_changed()
+
+    def _add_attribute_widgets(
+        self, name: str, *, editable: bool = False
+    ) -> None:
+        restore_button = QPushButton("Restore")
+        value_edit = QLineEdit("")
+        value_edit.setEnabled(editable)
+        layout = self._attribute_widget.layout()
+        row = layout.rowCount()
+        layout.addWidget(QLabel(name), row, 0)
+        layout.addWidget(value_edit, row, 1)
+        layout.addWidget(restore_button, row, 2)
+        self._value_edits[name] = value_edit
+        self._restore_buttons[name] = restore_button
+
+    def _on_selected_layers_changed(self) -> None:
+        if len(self.viewer.layers.selection) == 1:
+            self._show_selected_attributes()
+        else:
+            self._hide_attributes()
+
+    def _show_selected_attributes(self) -> None:
+        self._attribute_widget.show()
+        layer = next(iter(self.viewer.layers.selection))
+        for name in self._value_edits:
+            value = getattr(layer, name)
+            self._value_edits[name].setText(str(value))
+
+    def _hide_attributes(self) -> None:
+        self._attribute_widget.hide()
 
 
 @magic_factory
