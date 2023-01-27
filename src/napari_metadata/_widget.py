7e0aa806ac4b8e0988c7ce4abd7332f1ec81841c
Andy Sweet <andrew.d.sweet@gmail.com>
2023-01-27 11:39:33 -0800

    Handle changes to axis names

--- src/napari_metadata/_widget.py
+++ src/napari_metadata/_widget.py
@@ -21,7 +21,7 @@ from qtpy.QtWidgets import (
 )
 
 if TYPE_CHECKING:
-    from napari.components import ViewerModel
+    from napari.components import Dims, ViewerModel
     from napari.layers import Layer
 
 # Each layer metadata attribute must define a getter and setter.
@@ -243,10 +243,16 @@ class AxesWidget(AttributeWidget):
         viewer.dims.events.axis_labels.connect(
             self._on_viewer_dims_axis_labels_changed
         )
+        self._layer: Optional["Layer"] = None
+        self._viewer: "ViewerModel" = viewer
 
     def update(self, viewer: "ViewerModel", layer: "Layer") -> None:
-        self._update_num_axes(layer.ndim)
+        self._on_layer_changed(layer)
         self._set_axis_names(viewer.dims.axis_labels)
+
+    def _on_layer_changed(self, layer: "Layer") -> None:
+        self._layer = layer
+        self._update_num_axes(layer.ndim)
 
     def connect_layer(self, layer: "Layer") -> None:
         pass
@@ -258,12 +264,20 @@ class AxesWidget(AttributeWidget):
         num_widgets: int = self.layout().count() - 1
         # Add any missing widgets.
         for _ in range(num_axes - num_widgets):
-            self.layout().addWidget(AxisWidget(self))
+            widget = self._make_axis_widget()
+            self.layout().addWidget(widget)
         # Remove any unneeded widgets.
         for i in range(num_widgets - num_axes):
-            item: QLayoutItem = self.layout().takeAt(num_widgets - i)
+            # +1 because first widget is label
+            # TODO: better solution needed!
+            item: QLayoutItem = self.layout().takeAt(num_widgets - i + 1)
             # Need to unparent? Instead of deleting?
             item.widget().deleteLater()
+
+    def _make_axis_widget(self) -> None:
+        widget = AxisWidget(self)
+        widget.name.textChanged.connect(self._on_axis_name_changed)
+        return widget
 
     def _on_viewer_dims_axis_labels_changed(self, event) -> None:
         self._set_axis_names(event.value)
@@ -280,6 +294,15 @@ class AxesWidget(AttributeWidget):
             self.layout().itemAt(i).widget()
             for i in range(1, self.layout().count())
         ]
+
+    def _on_axis_name_changed(self) -> None:
+        names = [widget.name.text() for widget in self._widgets()]
+        dims: "Dims" = self._viewer.dims
+        all_labels = list(dims.axis_labels)
+        # Need noqa because pre-commit wants and doesn't want a space before
+        # the colon.
+        all_labels[-self._layer.ndim :] = names  # noqa
+        dims.axis_labels = all_labels
 
 
 class QMetadataWidget(QWidget):
