558c4faf179f4953d36aa16b4bfb5b8a8f3a8043
Andy Sweet <andrew.d.sweet@gmail.com>
2023-01-27 13:37:01 -0800

    Add init tests for all new widgets

--- src/napari_metadata/_tests/test_axis_type_units_widget.py
+++ src/napari_metadata/_tests/test_axis_type_units_widget.py
@@ -1,9 +1,9 @@
 from napari.components import ViewerModel
 
-from napari_metadata import QMetadataWidget
+from napari_metadata._axis_type_units_widget import AxesTypeUnitsWidget
 
 
 def test_init(qtbot):
     viewer = ViewerModel()
-    widget = QMetadataWidget(viewer)
+    widget = AxesTypeUnitsWidget(None, viewer)
     qtbot.addWidget(widget)
--- src/napari_metadata/_tests/test_axis_type_widget.py
+++ src/napari_metadata/_tests/test_axis_type_widget.py
@@ -1,9 +1,9 @@
 from napari.components import ViewerModel
 
-from napari_metadata import QMetadataWidget
+from napari_metadata._axis_type_widget import AxesTypeWidget
 
 
 def test_init(qtbot):
     viewer = ViewerModel()
-    widget = QMetadataWidget(viewer)
+    widget = AxesTypeWidget(None, viewer)
     qtbot.addWidget(widget)
