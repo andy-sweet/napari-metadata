f65d3346dc5d4aec74ca1197607c9291ff39586e
Andy Sweet <andrew.d.sweet@gmail.com>
2023-01-30 14:58:34 -0800

    Remove sub-widget tests

--- src/napari_metadata/_tests/test_axis_type_units_widget.py
+++
@@ -1,9 +0,0 @@
-from napari.components import ViewerModel
-
-from napari_metadata._axis_type_units_widget import AxesTypeUnitsWidget
-
-
-def test_init(qtbot):
-    viewer = ViewerModel()
-    widget = AxesTypeUnitsWidget(viewer)
-    qtbot.addWidget(widget)
